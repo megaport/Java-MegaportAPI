18e8726968127964f18018f57f1865832d969aeb
adam <adam@alandstreet.com>
2019-04-18 10:30:13 +1000

    Add in MCR version, add annotation to ignore unknown

--- src/main/java/com/megaport/api/dto/AvailableProductDto.java
+++ src/main/java/com/megaport/api/dto/AvailableProductDto.java
@@ -1,13 +1,17 @@
 package com.megaport.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AvailableProductDto implements Serializable {
 
     private Boolean mcr;
+    private Integer mcrVersion;
     private List<Integer> megaport = new ArrayList<>();
 
     public AvailableProductDto() {
@@ -23,6 +27,14 @@ public class AvailableProductDto implements Serializable {
 
     public List<Integer> getMegaport() {
         return megaport;
+    }
+
+    public Integer getMcrVersion() {
+        return mcrVersion;
+    }
+
+    public void setMcrVersion(Integer mcrVersion) {
+        this.mcrVersion = mcrVersion;
     }
 
     public void setMegaport(List<Integer> megaport) {

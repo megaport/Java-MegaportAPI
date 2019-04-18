0390de2863e5e753430f68aa6de0a51d9cb5b967
adam <adam@alandstreet.com>
2019-04-18 15:08:24 +1000

    random MAC

--- src/test/java/com/megaport/api/client/ServiceModificationTest.java
+++ src/test/java/com/megaport/api/client/ServiceModificationTest.java
@@ -99,13 +99,33 @@ public class ServiceModificationTest {
             dto.setProductUid(productUid);
             dto.setProductName("1234");
             dto.setAsn(1234L);
-            dto.setMacAddress("00-26-DD-14-C4-EE");
+            dto.setMacAddress(randomMACAddress());
             dto.setVlan(1234);
             dto.setRateLimit(1234);
             session.modifyIx(dto);
             IxServiceDto product = session.findServiceDetailIx(productUid);
             assertEquals("1234", product.getProductName());
         }
+    }
+
+    private String randomMACAddress(){
+        Random rand = new Random();
+        byte[] macAddr = new byte[6];
+        rand.nextBytes(macAddr);
+
+        macAddr[0] = (byte)(macAddr[0] & (byte)254);  //zeroing last 2 bytes to make it unicast and locally administered
+
+        StringBuilder sb = new StringBuilder(18);
+        for(byte b : macAddr){
+
+            if(sb.length() > 0)
+                sb.append(":");
+
+            sb.append(String.format("%02x", b));
+        }
+
+
+        return sb.toString();
     }
 
     @Test
